3460df26942f754a9ef745877dd29c4b558846c5
qchen3301 <q.chen.3301@gmail.com>
2018-08-30 00:43:18 -0400

    solved problem 2

--- hw.js
+++ hw.js
@@ -4,11 +4,21 @@
 
 var timerUI = {
   drawNumericDisplay: function(timerValue){
-    // Your Code Here
+    
     document.getElementById("numeric-display").textContent = timerValue
   },
   drawProgressBars: function(timerValue){
-    // Your Code Here
+  /*	I sussed out how to get this to run about 75% of the way but 
+  		got stuck on how to align the width expansion to the timer, so I 
+  		plugged in the rest from hw-solution.js. Questions:
+  			1. 	why an index position after getElementsByClassName()?
+  			2.	why is '%' a literal string? JS can interpret variable names
+  				representationally...but modifiers like % must be concat'd?? 
+  				Very confusing exception(s) to remember!	*/
+
+  		var progressBarExpand = 100 - timerValue
+  		document.getElementsByClassName("progress-bar")[0].style.width = progressBarExpand + '%'
+
   },
   drawLitFuses: function(timerValue){
     // Your Code Here
